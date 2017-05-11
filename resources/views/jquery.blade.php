4610ff8f5037a38c79f112931b71c939634e6f4f
allotmentandy <andy.derkacz@gmail.com>
2017-05-11 11:12:02 +0100

    markup fixes for putting the panel header in the right place :)

--- resources/views/jquery.blade.php
+++ resources/views/jquery.blade.php
@@ -12,6 +12,8 @@
             <div class="panel panel-default">     
                 <div class="panel-body">
                 Examples of Jquery i ike to use and have written
+                <br><br>
+                My <a href="https://allotmentandy.github.io/">blog</a> has a useful jquery class to make all links open in a new window, adding the target blank to all links!
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,13 +16,38 @@
                 </div>
             </div>
 
-        <div class="col-md-9">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    Welcome to my demo laravel site. 
-                    <br>
-                    There are a number of artisan commands to download and import data. Please see the readme.md file
-                
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        Welcome to my demo laravel site. 
+                        <br>
+                        There are a number of artisan commands to download and import data. Please see the readme.md file
+                        <h4>Also see Andy's BLOG</h4>
+                        <p>
+                        Please visit my <a href="https://allotmentandy.github.io/" target="_blank">https://allotmentandy.github.io/</a> pages for useful links and info about php, laravel, html and other stuff i am interested in.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Planes</div>
+                    <div class="panel-body">
+                        <div class="panel-body">
+                            I am a fan of private aviation, this is the start of a tool to help log 'bizjets' you have seen. 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Londinium</div>
+                    <div class="panel-body">
+                        <div class="panel-body">
+                            Tool to manage a website link directory.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
