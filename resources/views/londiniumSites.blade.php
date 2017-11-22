f085d9add5fc2c925549014139fad67dcd1a3eef
allotmentandy <andy.derkacz@gmail.com>
2017-11-22 08:10:35 +0000

    table css rollover and UI improvements for the directory manager pages

--- resources/views/londiniumSites.blade.php
+++ resources/views/londiniumSites.blade.php
@@ -10,39 +10,29 @@
                 <br>
                 highest id saved: {{$highestSavedId}}
             </div>
-
         <div class="col-md-8">
             <div class="panel panel-default">
-                
                 <div class="panel-body">
                     <table border="1">
-
                     @foreach ($sites as $site)
-                    
-                    <?php
-                    echo "<tr><td>";
-                    if ($site->saved == 'saved') {
-                        echo("SAVED ");
-                    } else {
-                        echo "<a href=\"/londinium/sites/save/$site->id\" class=\"button\">Save</a> ";
-                    }
-                    ?>
-                    @if ($site->name)
-                        {{$site->name}}
-                    @else 
-                        {{$site->url}}
-                    @endif       
-                    </td><td>
-                    <a href="/londinium/site/{{$site->id}}" target="_blank">details </a>
-                    </td><td>
-
-                    <?php
-                    if ($site->saved == 'saved') {
-                        echo "<a href=\"/londinium/sites/unsave/$site->id\" class=\"button\">UNSAVE</a> ";
-                    }
-                    echo "</td></tr>";
-                    ?>
-
+                        <?php
+                        echo "<tr><td>";
+                        if ($site->saved == 'saved') {
+                            echo("SAVED ");
+                        } else {
+                            echo "<a href=\"/londinium/sites/save/$site->id\" class=\"button\">Save</a> ";
+                        }
+                        ?>
+                        </td><td>
+                        @if ($site->name)
+                            {{$site->name}}
+                        @else 
+                            {{$site->url}}
+                        @endif       
+                        </td><td>
+                        <a href="/londinium/site/{{$site->id}}" target="_blank">details </a>
+                        </td></tr>
+                        
                     @endforeach
                     </table>    
 
