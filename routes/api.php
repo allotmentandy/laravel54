3eee3f352afa4f405364ff70fbec264b5f67715a
andy <andy.derkacz@gmail.com>
2017-12-27 19:46:05 +0000

    added a first api ROUTE into the api.routes file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,8 +9,8 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/getRegistrations', function () {
+    return "ok";
 });
