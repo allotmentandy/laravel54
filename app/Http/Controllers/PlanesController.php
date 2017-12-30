cfde93b9c60194d559c2efe34a301ce1a674ecd5
andy <andy.derkacz@gmail.com>
2017-12-30 23:13:46 +0000

    made random images airbus a319 - need to put this in the settings

--- app/Http/Controllers/PlanesController.php
+++ app/Http/Controllers/PlanesController.php
@@ -21,7 +21,9 @@ class PlanesController extends Controller
 //        $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Dassault Falcon 900EX')->take(1)->get();
         // $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Dassault Falcon 900')->take(1)->get();
         // $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Gulfstream IV')->take(1)->get();
-        $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Gulfstream G450')->take(1)->get();
+
+        $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Airbus A319CJ')->take(1)->get();
+//        $data['details'] = Planes::orderByRaw('RAND()')->where('type', '=', 'Gulfstream G450')->take(1)->get();
 
         return view('planes', $data);
     }
