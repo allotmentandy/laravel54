b603dfe865a3a303eb2759f88cc81786fee4dcfb
allotmentandy <andy.derkacz@gmail.com>
2018-01-16 21:14:12 +0000

    updated the frontpage and screenshots

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,15 +57,15 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ route('planes') }}"><button type="button" class="btn btn-primary btn-sm"> <i class="fas fa-plane fa-2x"></i> Private JETS</button></a></li>
+                    <li><a href="{{ route('planes') }}"><button type="button" class="btn btn-primary btn-sm"><i class="fas fa-plane fa-2x"></i> Private JETS</button></a></li>
 
-                    <li><a href="{{ route('londinium') }}"><button type="button" class="btn btn-primary btn-sm"> <i class="fab fa-linode fa-2x"></i> Londinium</button></a></li>
+                    <li><a href="{{ route('londinium') }}"><button type="button" class="btn btn-primary btn-sm"><i class="fab fa-linode fa-2x"></i> Londinium</button></a></li>
 
-                    <li><a href="{{ route('vue') }}"><button class="btn btn-primary btn-sm"><i class="fab fa-vuejs fa-2x"></i>Vue.js</button></li></a>
+                    <li><a href="{{ route('vue') }}"><button class="btn btn-primary btn-sm"><i class="fab fa-vuejs fa-2x"></i> Vue.js</button></li></a>
 
-                    <li><a href="{{ route('rss') }}"><button class="btn btn-primary btn-sm"><i class="fas fa-rss fa-2x"></i>RSS</button></li></a>
+                    <li><a href="{{ route('rss') }}"><button class="btn btn-primary btn-sm"><i class="fas fa-rss fa-2x"></i> RSS</button></li></a>
 
-                    <li><a href="{{ route('jquery') }}"><button class="btn btn-primary btn-sm"><i class="fab fa-node-js fa-2x"></i>Jquery</button></li></a>
+                    <li><a href="{{ route('jquery') }}"><button class="btn btn-primary btn-sm"><i class="fab fa-node-js fa-2x"></i> Jquery</button></li></a>
 
 <!--                     <li><a href="{{ route('cssE') }}"><button class="btn btn-primary btn-sm"><i class="fab fa-css3"></i>css</button></li></a>
  -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,57 +3,76 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-                <div class="panel-heading">Examples</div>
-                    <div class="panel-body">
-                    <a href="{{ route('rssJobs') }}">RSS job feeds</a><br>
-                    <a href="{{ route('rssNews') }}">RSS news feeds</a><br>
-                    <a href="{{ route('jquery') }}">Jquery Examples</a><br>
-                    <a href="{{ route('vue') }}">Vue Examples</a><br>
-                    <a href="{{ route('cssE') }}">CSS Examples</a><br>
-                    </div>
-                <div>
-                    <i class="fab fa-vuejs fa-3x"></i>
-                    <i class="fab fa-github fa-3x"></i>
-                    <i class="fab fa-twitter fa-3x"></i>
-                    <i class="fab fa-youtube fa-3x"></i>
-                    <i class="fab fa-laravel fa-3x"></i>
+            <div class="panel panel-primary">
+                <div class="panel-heading"><i class="fas fa-code fa-2x"></i>Examples</div>
+                <div class="panel-body">
+                    <a href="{{ route('planes') }}"><i class="fas fa-plane fa-2x"></i> Private Jet Tools</a><br>
+                    <a href="{{ route('londinium') }}"><i class="fab fa-linode fa-2x"></i> Londinium directory</a><br>
+                    <a href="{{ route('rssJobs') }}"><i class="fas fa-rss fa-2x"></i> RSS job feeds</a><br>
+                    <a href="{{ route('rssNews') }}"><i class="fas fa-rss fa-2x"></i> RSS news feeds</a><br>
+                    <a href="{{ route('jquery') }}"><i class="fab fa-node-js fa-2x"></i> Jquery Examples</a><br>
+                    <a href="{{ route('vue') }}"><i class="fab fa-vuejs fa-2x"></i> Vue Examples</a><br>
+                    <!-- <a href="{{ route('cssE') }}"><i class="fab fa-css3"></i>CSS Examples</a><br> -->
                 </div>
+            </div>
         </div>
-        <div class="col-md-8">
+        <div class="col-md-9">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="panel panel-primary">
+                    <div class="panel panel-default">
                         <div class="panel-body">
-                            <div class="panel-body" style="background:black; color: green;">
-                                <div class="css-typing">
-                                    <p>
-                                        Love Laravel <i class="fab fa-laravel fa-lg"></i>
-                                    </p>
-                                    <p>
-                                        <i class="fa fa-plane fa-lg"></i>  Private Jets!
-                                    </p>
-                                    <p>
-                                        what more u want?
-                                    </p>
-                                    <img src="/images/andy.jpg">
+                            <img src="/images/andy.jpg" style="width:100%">    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-body" style="background:black; color: green;">
+                            <div class="css-typing">
+                                <p>
+                                    Love Laravel <i class="fab fa-laravel fa-lg"></i>
+                                </p>
+                                <p>
+                                    <i class="fa fa-plane fa-lg"></i>  Private Jets!
+                                </p>
+                                <p>
+                                    what more do you want?
+                                </p>                                    
                             </div>
+                        </div>
+
+                        <div class="panel-body">
+                        
+                            <p>I am a London based full stack developer looking for a new role.</p>
+                            <h4>Technology Stack</h4>
+                            <ul>
+                                <li>PHP</li>
+                                <li>MySql</li>
+                                <li>Laravel</li>
+                                <li>Packagist</li>
+                                <li>Vue.js</li>
+                                <li>Bootstrap CSS</li>
+                                <li>jQuery</li>
+                                <li>Web Spiders</li>
+                                <li>Shell Scripting</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-                <div class="col-md-6">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                        There are a number of artisan commands to download and import data. Please see the readme.md file for more details on how to get it setup.
-                        <h6>Private Jet Database Tools</h6>
-                        I am a fan of private aviation, this is the start of a tool to help log 'bizjets' you have seen.
-                        <h6>Londinium website directory</h6>
-                        Tool to manage a website link directory
-                        </div>
-                    </div>
-                </div>
-
         </div>
     </div>
-</div>
-@endsection
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <h4>Get in contact  </h4>
+                    <hr>
+                    <i class="fab fa-github fa-1x"></i> github.com/allotmentandy 
+                    <i class="fab fa-github fa-1x"></i> allotmentandy.github.io 
+                    <i class="fab fa-twitter fa-1x"></i> twitter.com/londiniumcom
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
